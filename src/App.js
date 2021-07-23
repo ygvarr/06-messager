a771f2b6834341f5b6b8c008d8d9e1cef08a227b
ygvarr <ygvarr@gmail.com>
2021-07-24 00:22:45 +0300

    99

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Music from './Components/Music/Music'
 import News from './Components/News/News'
 import Settings from './Components/Settings/Settings'
-import {BrowserRouter, Route, withRouter} from 'react-router-dom'
+import {BrowserRouter, Redirect, Route, Switch, withRouter} from 'react-router-dom'
 import DialogsContainer from './Components/Dialogs/DialogsContainer'
 import UsersContainer from './Components/Users/UsersContainer'
 import ProfileContainer from './Components/Profile/ProfileContainer'
@@ -31,17 +31,21 @@ class App extends React.Component {
                 <HeaderContainer className='Header'/>
                 <Navbar className='Navbar'/>
                 <div className='Content'>
-                    <Route path='/profile/:userId?'
-                           render={withSuspense(ProfileContainer)}/>
-                    <Route path='/im'
-                           render={withSuspense(DialogsContainer)}/>
-                    <Route path='/users'
-                           render={withSuspense(UsersContainer)}/>
-                    <Route path='/login'
-                           render={() => <LoginPage/>}/>
-                    <Route path='/feed' render={News}/>
-                    <Route path='/music' render={Music}/>
-                    <Route path='/settings' render={Settings}/>
+                    <Switch>
+                        <Route exact path='/' render={() => <Redirect to={'/profile'}/>}/>
+                        <Route path='/profile/:userId?'
+                               render={withSuspense(ProfileContainer)}/>
+                        <Route path='/im'
+                               render={withSuspense(DialogsContainer)}/>
+                        <Route path='/users'
+                               render={withSuspense(UsersContainer)}/>
+                        <Route path='/login'
+                               render={() => <LoginPage/>}/>
+                        <Route path='/feed' render={News}/>
+                        <Route path='/music' render={Music}/>
+                        <Route path='/settings' render={Settings}/>
+                        <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
+                    </Switch>
                 </div>
             </div>
         )
