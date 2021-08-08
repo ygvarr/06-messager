920f729d066fffd421c33bd16e6baa5bd7c822ef
ygvarr <ygvarr@gmail.com>
2021-08-09 00:13:02 +0300

    107 ts

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,5 +1,5 @@
 import {stopSubmit} from 'redux-form'
-import {authAPI, securityAPI} from '../api/api'
+import {authAPI, ResultCodesEnum, securityAPI} from '../api/api'
 
 const SET_USER_DATA = 'auth/SET_USER_DATA'
 const GET_CAPTCHA_URL_SUCCESS = 'auth/GET_CAPTCHA_URL_SUCCESS'
@@ -51,26 +51,26 @@ export const getCaptchaUrlSuccess = (captchaUrl: string): GetCaptchaUrlSuccessTy
 )
 export const getAuthUserData = () => async (dispatch: any) => {
     const response = await authAPI.me()
-    if (response.data.resultCode === 0) {
-        let {id, email, login} = response.data.data
+    if (response.resultCode === ResultCodesEnum.Success) {
+        let {id, email, login} = response.data
         dispatch(setAuthUserData(id, email, login, true))
     }
 }
 export const login = (email: string, password: string, rememberMe: boolean, captcha: string) => async (dispatch: any) => {
     const response = await authAPI.login(email, password, rememberMe, captcha)
-    if (response.data.resultCode === 0) {
+    if (response.resultCode === ResultCodesEnum.Success) {
         dispatch(getAuthUserData())
     } else {
-        if (response.data.resultCode === 10) {
+        if (response.resultCode === ResultCodesEnum.CaptchaIsRequired) {
             dispatch(getCaptchaUrl())
         }
-        const message = response.data.messages.length > 0 ? response.data.messages[0] : 'Unknown error'
+        const message = response.messages.length > 0 ? response.messages[0] : 'Unknown error'
         dispatch(stopSubmit('login', {_error: message}))
     }
 }
 export const logout = () => async (dispatch: any) => {
     const response = await authAPI.logout()
-    if (response.data.resultCode === 0) {
+    if (response.data.resultCode === ResultCodesEnum.Success) {
         dispatch(setAuthUserData(null, null, null, false))
     }
 }
